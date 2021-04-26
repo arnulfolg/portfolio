8f7ea79943bc07b2be6fdd2a83af5ae4745ade5e
Arnulfo Loredo <arnulfo.lg@live.com>
2021-04-26 14:19:45 -0700

    Added markdown renderer

--- src/views/project/Project.js
+++ src/views/project/Project.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
 
 import { useContentful } from "../../Hooks/useContentful";
 import { PROJECT_QUERY } from "../../graphql/queries";
@@ -10,26 +11,32 @@ function Project() {
 
 	return (
 		<>
-			<header className="section">
+			<header className="">
 				<h1>{content?.portfolio?.projectName}</h1>
+				<section className="section summary">
+					<section className="summary_info summary_info__left">
+						<p className="summary_title">
+							{content?.client?.heading}
+						</p>
+						<p>{content?.portfolio?.client}</p>
+					</section>
+					<section className="summary_info">
+						<p className="summary_title">
+							{content?.role?.heading}
+						</p>
+						<p>{content?.portfolio?.role}</p>
+					</section>
+					<section className="summary_info summary_info__right">
+						<p className="summary_title">
+							{content?.time?.heading}
+						</p>
+						<p>{content?.portfolio?.time}</p>
+					</section>
+				</section>
 			</header>
-			<section className="section summary">
-				<section className="summary_info summary_info__left">
-					<p className="summary_title">{content?.client?.heading}</p>
-					<p>{content?.portfolio?.client}</p>
-				</section>
-				<section className="summary_info">
-					<p className="summary_title">{content?.role?.heading}</p>
-					<p>{content?.portfolio?.role}</p>
-				</section>
-				<section className="summary_info summary_info__right">
-					<p className="summary_title">{content?.time?.heading}</p>
-					<p>{content?.portfolio?.time}</p>
-				</section>
-			</section>
 			<section className="section section_left">
 				<h2>{content?.summary?.heading}</h2>
-				<p>{content?.portfolio?.summary}</p>
+				<ReactMarkdown children={content?.portfolio?.summary} />
 				<img src={content?.portfolio?.summaryPicture.url} alt="" />
 			</section>
 			<section className="section section_left">
@@ -38,7 +45,7 @@ function Project() {
 			</section>
 			<section className="section section_left">
 				<h2>{content?.description?.heading}</h2>
-				<p>{content?.portfolio?.description}</p>
+				<ReactMarkdown children={content?.portfolio?.description} />
 			</section>
 			<section className="section section_left">
 				<h2>{content?.gallery?.heading}</h2>
@@ -56,10 +63,12 @@ function Project() {
 					)}
 				</picture>
 			</section>
-			<section className="section section_left">
-				<h2>{content?.takeaway?.heading}</h2>
-				<p>{content?.portfolio?.takeaway}</p>
-			</section>
+			<footer>
+				<section className="section section_left">
+					<h2>{content?.takeaway?.heading}</h2>
+					<ReactMarkdown children={content?.portfolio?.takeaway} />
+				</section>
+			</footer>
 		</>
 	);
 }

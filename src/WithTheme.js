405230228b80a82f577375f4f02a13290cedc039
Arnulfo Loredo <arnulfo.lg@live.com>
2021-05-01 17:32:40 -0700

    Added localstorage support to save user preferences

--- src/WithTheme.js
+++ src/WithTheme.js
@@ -3,15 +3,37 @@ import ThemeContext from "./ThemeContext";
 
 const WithTheme = (OriginalComponent) => {
 	function NewComponent() {
-		const [gettheme, settheme] = useState("light");
-		const [getlocale, setlocale] = useState("en-US");
+		const [gettheme, settheme] = useState(() => {
+			const theme = window.localStorage.getItem("theme") || "light";
+			return theme;
+		});
+		const [getlocale, setlocale] = useState(() => {
+			const locale = window.localStorage.getItem("locale") || "en-US";
+			return locale;
+		});
 
 		const toggleTheme = () => {
-			gettheme === "light" ? settheme("dark") : settheme("light");
+			gettheme === "light"
+				? settheme(() => {
+						window.localStorage.setItem("theme", "dark");
+						return "dark";
+				  })
+				: settheme(() => {
+						window.localStorage.setItem("theme", "light");
+						return "light";
+				  });
 		};
 
 		const toggleLocale = () => {
-			getlocale === "en-US" ? setlocale("es-419") : setlocale("en-US");
+			getlocale === "en-US"
+				? setlocale(() => {
+						window.localStorage.setItem("locale", "es-419");
+						return "es-419";
+				  })
+				: setlocale(() => {
+						window.localStorage.setItem("locale", "en-US");
+						return "en-US";
+				  });
 		};
 
 		return (

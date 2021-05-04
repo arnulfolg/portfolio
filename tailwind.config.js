2fc49727424edf5ea44755cb532fdb68d9b37107
Arnulfo Loredo <arnulfo.lg@live.com>
2021-05-04 16:42:15 -0700

    Custom colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: "class", // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				// yellow: {
+				// 	100: "#FFE15C",
+				// 	200: "#FFDC42",
+				// 	300: "#FFD829",
+				// 	400: "#FFD30F",
+				// 	500: "#F0C400",
+				// },
+				yellow: {
+					100: "#FFE675",
+					200: "#FFDC42",
+					300: "#FFD729",
+					400: "#FFD20E",
+					500: "#EFB106",
+				},
+			},
+		},
+		fontFamily: {
+			sans: ["IBM Plex Sans", "Helvetica", "sans-serif"],
+			serif: ["Rubik", "serif"],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};

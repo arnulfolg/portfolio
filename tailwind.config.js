0d1e192dae7fb1f307ed5a39ec136c745ae8bf59
Arnulfo Loredo <arnulfo.lg@live.com>
2021-05-04 23:50:43 -0700

    Added custom colors and enhanced dark mode with darker gray color in bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,20 @@ module.exports = {
 				// 	500: "#F0C400",
 				// },
 				yellow: {
-					100: "#FFE675",
-					200: "#FFDC42",
-					300: "#FFD729",
+					100: "#FFEFA8",
+					200: "#FFE675",
+					300: "#FFDC42",
 					400: "#FFD20E",
-					500: "#EFB106",
+					500: "#D18100",
+				},
+				gray: {
+					900: "#191919",
 				},
 			},
 		},
 		fontFamily: {
-			sans: ["IBM Plex Sans", "Helvetica", "sans-serif"],
-			serif: ["Rubik", "serif"],
+			ibm: ["IBM Plex Sans", "Helvetica", "sans-serif"],
+			rubik: ["Rubik", "serif"],
 		},
 	},
 	variants: {

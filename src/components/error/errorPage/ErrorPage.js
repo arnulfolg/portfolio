44c234c4e38c3f967c912031b701258b554fae00
Arnulfo Loredo <arnulfo.lg@live.com>
2021-05-06 16:10:06 -0700

    Error Page css styles

--- src/components/error/errorPage/ErrorPage.js
+++ src/components/error/errorPage/ErrorPage.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ErrorPage.css";
 
 function ErrorPage() {
 	return (
-		<section>
+		<section className="section error_page">
 			<h2>Project Not Found</h2>
 			<p>
 				It seems the project you are looking for doesn't exist or it was
-				removed
+				removed by our Cat Admin
 			</p>
-			<Link to="/portfolio">Go to Projects</Link>
+			<img
+				className="img"
+				src="https://images.ctfassets.net/ui8qz5ptyq23/H5uum9ifZ80EIVFAxgX1j/660429f28f88934f7811c45e5029b453/599.jpg"
+				alt=""
+			/>
+			<Link className="button" to="/portfolio">
+				Go to Projects
+			</Link>
 		</section>
 	);
 }

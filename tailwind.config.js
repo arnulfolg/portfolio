fb48116971792d366e6060c3160dcd786b778944
Arnulfo Loredo <arnulfo.lg@live.com>
2024-01-30 13:13:40 -0800

    Added back custom typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				yellow: {
+					100: "#FFEFA8",
+					200: "#FFE675",
+					300: "#FFDC42",
+					400: "#FFD20E",
+					500: "#D18100",
+				},
+				gray: {
+					900: "#191919",
+				},
+			},
+		},
+		fontFamily: {
+			ibm: ["IBM Plex Sans", "Helvetica", "sans-serif"],
+			rubik: ["Rubik", "serif"],
+		},
 	},
 	plugins: [],
 };
